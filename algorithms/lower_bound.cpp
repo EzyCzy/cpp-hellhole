a7154ef61ae4549a9635b3730533af599321f22e
EzyCzy <mirulh406@gmail.com>
2025-01-10 23:11:16 +1100

    feat: pushing lower_bound

--- algorithms/lower_bound.cpp
+++ algorithms/lower_bound.cpp
@@ -1,1 +1,26 @@
-#include <iostream>
+#include <vector>
+
+// returns the index of the element that is equal or larger than target
+int lower_bound(const std::vector<int>& arr, const int target) {
+
+    int sz = static_cast<int>(arr.size());
+    int lo = 0;
+    int hi = sz;
+
+    while (lo < hi) {
+        int mid = lo + (hi - lo)/2;
+
+        if (target <= arr[mid]) {
+            // hi set to mid so we dont lose element if matches target
+            hi = mid;
+        } else {
+            lo = mid + 1;
+        }
+    }
+
+    if (lo < sz && arr[lo] < target) {
+        lo++;
+    }
+
+    return lo;
+}
